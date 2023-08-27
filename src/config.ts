1b3f3f94cbd5b07b6d6815e07520741429f38599
letieu <letieu8@gmail.com>
2023-08-27 11:09:38 +0700

    update gitignore

---
+++ src/config.ts
@@ -0,0 +1,27 @@
+export default {
+  response_timeout: 10000,
+  headless: false,
+  base_url: "https://www.facebook.com/",
+  username_field: 'input[name="email"]',
+  password_field: 'input[name="pass"]',
+  login_button: 'button[type="submit"]',
+  username_invalid_msg:
+    "The email address or mobile number you entered isn't connected to an account.",
+  password_invalid_msg: "The password that you've entered is incorrect",
+  credentials_invalid_msg_01: "Wrong Credentials",
+  credentials_invalid_msg_02: "Invalid username or password",
+  regex_remove_non_numeric: /[^0-9]/g,
+  page_end_msg: "search results only include things visible to you.",
+  page_not_available_msg: "this page isn't available",
+  content_not_available_msg: "this content isn't available right now",
+  content_not_available_msg_02: "this content isn't available at the moment",
+  content_not_available_msg_03: "we didn't find anything",
+  user_profile_check_msg: "add friend",
+  no_results_msg: "we didn't find any results",
+  end_of_results: "end of results",
+  date_time_format: "DD/MM/YYYY HH:mm:ss",
+  close_button_selector: "div[aria-label='Close']",
+  seven_days_timestamp: 7 * 24 * 60 * 60 * 1000,
+  login_button_name: "log in",
+  public_post_exclude_keyword: "see all public posts for"
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,141 @@
-console.log('hehe');
+import 'dotenv/config';
+import puppeteer, { Browser, Page } from 'puppeteer';
+import config from './config';
+
+const POST_URL = 'https://mbasic.facebook.com/groups/817474248860972/posts/1373517816589943/'
+const CHROME_PROFILE_ID = 1
+
+class Crawler {
+  browser: Browser;
+  page: Page;
+
+  async initPuppeter() {
+    this.browser = await puppeteer.launch({
+      headless: config.headless,
+      args: ["--no-sandbox"],
+      userDataDir: `./profile/${CHROME_PROFILE_ID}`,
+      devtools: false,
+    });
+    const context = this.browser.defaultBrowserContext();
+    context.overridePermissions(config.base_url, [
+      "geolocation",
+      "notifications",
+    ]);
+    this.page = await this.browser.newPage();
+    this.page.setViewport({ width: 1500, height: 764 });
+  }
+
+  async loginFacebook(user_name, password) {
+    await this.page.goto(config.base_url, { waitUntil: "networkidle2" });
+    await this.page.waitForSelector(config.username_field, {
+      timeout: config.response_timeout,
+    });
+    await this.page.type(config.username_field, user_name, { delay: 50 });
+    await this.page.type(config.password_field, password, { delay: 50 });
+    await this.page.click(config.login_button);
+    await this.page.waitForNavigation({ waitUntil: "networkidle2" });
+    await this.page.waitForTimeout(1000 + Math.floor(Math.random() * 500));
+    let bodyHTML = await this.page.evaluate(() => document.body.innerHTML);
+    let bodyString = bodyHTML.toString();
+    let loginResponse: any = {};
+
+    if (bodyString.includes(config.username_invalid_msg)) {
+      loginResponse.authenticated = false;
+      loginResponse.user = false;
+    } else if (bodyString.includes(config.password_invalid_msg)) {
+      loginResponse.authenticated = false;
+      loginResponse.user = true;
+    } else if (
+      bodyString.includes(config.credentials_invalid_msg_01) ||
+      bodyString.includes(config.credentials_invalid_msg_02)
+    ) {
+      loginResponse.authenticated = false;
+      loginResponse.user = false;
+    } else {
+      loginResponse.success = true;
+      loginResponse.user = true;
+      loginResponse.authenticated = true;
+    }
+    await this.page.waitForTimeout(500 + Math.floor(Math.random() * 500));
+    return loginResponse;
+  }
+
+  async start() {
+    try {
+      await this.initPuppeter();
+
+      // TODO: check login
+      // const loginResponse = await crawler.loginFacebook(process.env.EMAIL, process.env.PASS);
+      // if (!loginResponse.success) {
+      //   throw new Error('Login failed');
+      // }
+
+      await this.page.goto(POST_URL, { waitUntil: "networkidle2" });
+
+      const postContent = await this.getPostContent();
+      const comments = await this.getComments();
+
+      console.log(postContent, 'postContent');
+      console.log(comments, 'comments');
+
+    } catch (error) {
+      console.log(error);
+    } finally {
+      // await this.browser.close();
+    }
+  }
+
+  async getComments() {
+    const allComments = [];
+
+    while (true) {
+      const comments = await this.page.evaluate(() => {
+        const commentNodes = document.querySelectorAll(".e.ch.ci .ec.cj .ed:not(.ew):not(.ee)");
+
+        const comments = Array.from(commentNodes).map((commentNode) => {
+          const comment = {
+            user: commentNode.querySelector("h3 > a")?.innerHTML,
+            content: commentNode.querySelector(".ef")?.innerHTML,
+            timestamp: commentNode.querySelector(".ba > abbr")?.innerHTML,
+            id: commentNode.id,
+          };
+          return comment;
+        });
+
+        return comments;
+      });
+
+      allComments.push(...comments);
+
+      const loadMoreSelector = "[id^=see_next_] > a";
+      const loadMoreLink = await this.page.$(loadMoreSelector);
+
+      if (loadMoreLink) {
+        await loadMoreLink.click();
+        await this.page.waitForTimeout(1000 + Math.floor(Math.random() * 500));
+      }
+      else {
+        break;
+      }
+    }
+
+    return allComments;
+  }
+
+  async getPostContent() {
+    const postContent = await this.page.evaluate(() => {
+      const postContent = document
+        .querySelector("div.bl > div.bu > div > p")
+
+      return postContent.innerHTML
+    }).catch((error) => {
+      console.log(error);
+      return null;
+    });
+
+    return postContent;
+  }
+}
+
+const crawler = new Crawler();
+crawler.start();
