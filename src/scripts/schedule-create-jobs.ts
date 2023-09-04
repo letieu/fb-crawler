ad453f899c6a58ba40aad96ec5bd705a3a49518b
letieu <letieu8@gmail.com>
2023-09-04 18:56:11 +0700

    run cron on init

--- src/scripts/schedule-create-jobs.ts
+++ src/scripts/schedule-create-jobs.ts
@@ -2,21 +2,23 @@ import 'dotenv/config';
 import { triggerCrawl } from '../queues/crawl-queue';
 import { CronJob } from 'cron';
 
+const cronTime = process.env.CRAWL_CRON || '* 0 */8 * * *';
+
+const job = new CronJob(
+  cronTime,
+  async () => {
+    await triggerCrawl();
+    console.log('triggerCrawl from cron');
+  },
+  null, // onComplete
+  false,
+  'Asia/Ho_Chi_Minh',
+  null, // context
+  true, // runOnInit
+);
+
 async function main() {
-  const cronTime = process.env.CRAWL_CRON || '* 0 */8 * * *';
-
-  const job = new CronJob(
-    cronTime,
-    async () => {
-      await triggerCrawl();
-      console.log(`Trigger crawl by schedule at ${new Date()}`);
-    },
-    null,
-    true,
-  );
-
   job.start();
-
   console.log(`Crawl cron: ${cronTime} started`);
 }
 
