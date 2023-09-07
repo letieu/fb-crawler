8e484c2c63f11826704690b8a1b10d93709eaec2
letieu <letieu8@gmail.com>
2023-09-07 22:17:11 +0700

    sleep 20s for post id worker

--- src/scripts/workers/post-id.ts
+++ src/scripts/workers/post-id.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { startPostIdWorker } from '../../workers/post-ids-worker';
 
 async function main() {
-  await new Promise((resolve) => setTimeout(resolve, 10000)); // sleep 10s
+  await new Promise((resolve) => setTimeout(resolve, 20 * 1000)); // sleep 20s
   await startPostIdWorker();
 }
 
