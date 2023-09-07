237dce28bbdaa3168b6e9e1a3c2cd90fdcdd9532
letieu <letieu8@gmail.com>
2023-09-07 22:15:05 +0700

    sleep 10s before start worker

--- src/scripts/workers/post-detail.ts
+++ src/scripts/workers/post-detail.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { startPostDetailWorker } from '../../workers/post-detail-worker';
 
 async function main() {
+  await new Promise((resolve) => setTimeout(resolve, 10000)); // sleep 10s
   await startPostDetailWorker();
 }
 
--- src/scripts/workers/post-id.ts
+++ src/scripts/workers/post-id.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { startPostIdWorker } from '../../workers/post-ids-worker';
 
 async function main() {
+  await new Promise((resolve) => setTimeout(resolve, 10000)); // sleep 10s
   await startPostIdWorker();
 }
 
