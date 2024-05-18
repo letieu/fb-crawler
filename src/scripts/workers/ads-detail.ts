caeff6c08e0767b73ad15cf896eceeb3a3f9e080
letieu <letieu8@gmail.com>
2024-05-18 15:06:57 +0700

    feat: add ads detail crawler

--- src/scripts/workers/ads-detail.ts
+++ src/scripts/workers/ads-detail.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { } from '../../workers/post-detail-worker';
+import { startAdsDetailWorker } from '../../workers/ads-detail-worker';
 
 async function main() {
   await new Promise((resolve) => setTimeout(resolve, 10000)); // sleep 10s
